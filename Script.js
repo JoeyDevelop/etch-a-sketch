96ae7dcd16273039876bb8bb35bb74e9c1836830
Joey Patterson <JoeyPattersonBusiness@gmail.com>
2022-10-08 21:52:35 -0400

    set container to be non-flexible; add basic button pseudocode

--- Script.js
+++ Script.js
@@ -13,4 +13,12 @@ function makeRows(rows, cols) {
   }
 }
 
-makeRows(16, 16);
+//makeRows(100, 100);
+
+// const gridbtn = document.querySelector("#grid-question");
+// gridbtn.addEventListener("click", () => {
+//     let answer = prompt("Please input the desired number of rows and columns. 100 is the highest accepted value.");
+//     console.log(answer);
+// });
+
+//console.log(answer);
